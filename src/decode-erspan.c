b037c2c8cb0836b17ae853b05812f525960d6917
Jason Ish <jason.ish@oisf.net>
2019-11-04 15:42:12 -0600

    src: move util sources into util/
    Redmine issue:
    https://redmine.openinfosecfoundation.org/issues/3166

--- src/decode-erspan.c
+++ src/decode-erspan.c
@@ -36,8 +36,8 @@
 #include "decode-events.h"
 #include "decode-erspan.h"
 
-#include "util-unittest.h"
-#include "util-debug.h"
+#include "util/util-unittest.h"
+#include "util/util-debug.h"
 
 /**
  * \brief Functions to decode ERSPAN Type I and II packets
