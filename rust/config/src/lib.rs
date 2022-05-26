5e1757d85b3feec5ceb51e5c4673a9b300891add
Jason Ish <jason.ish@oisf.net>
2022-05-26 17:07:33 -0600

    config: simple getter

--- rust/config/src/lib.rs
+++ rust/config/src/lib.rs
@@ -19,3 +19,43 @@ pub mod loader;
 
 pub use crate::loader::LoaderError;
 pub use yaml_rust::Yaml;
+
+pub fn get_node<'a>(node: &'a Yaml, key: &str) -> Option<&'a Yaml> {
+    let parts: Vec<&str> = key.splitn(2, '.').collect();
+    if parts.is_empty() {
+        return None;
+    }
+    if let Yaml::Hash(hash) = node {
+        let key = Yaml::from_str(parts[0]);
+        if let Some(node) = hash.get(&key) {
+            if parts.len() == 1 {
+                return Some(node);
+            } else {
+                return get_node(node, parts[1]);
+            }
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_node() {
+        let doc = r#"
+        simple: value
+        nested:
+            aaa: bbb
+        "#;
+
+        let config = loader::load_from_str(doc).unwrap().pop().unwrap();
+
+        let node = get_node(&config, "simple").unwrap();
+        assert_eq!(node.as_str().unwrap(), "value");
+
+        let node = get_node(&config, "nested.aaa").unwrap();
+        assert_eq!(node.as_str().unwrap(), "bbb");
+    }
+}
