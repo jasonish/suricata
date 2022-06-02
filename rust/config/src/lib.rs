9806c21cdb51f1fe2b29a1de78ce77d45f9fa158
Jason Ish <jason.ish@oisf.net>
2022-06-02 17:31:22 -0600

    rust/config: start on C api (arrays, hashes)
    Provide C functions for iterating Rust YAML arrays and hashes.

--- rust/config/src/lib.rs
+++ rust/config/src/lib.rs
@@ -25,7 +25,76 @@ use std::sync::RwLock;
 pub use yaml_rust::Yaml;
 
 lazy_static! {
-    static ref DEFAULT: RwLock<Yaml> = RwLock::new(Yaml::Hash(LinkedHashMap::new()));
+    static ref GLOBAL: RwLock<Yaml> = RwLock::new(Yaml::Hash(LinkedHashMap::new()));
+}
+
+pub trait SuricataYaml {
+    fn set_int(&mut self, key: &str, value: i64) -> bool;
+    fn set_from_str(&mut self, key: &str, value: &str) -> bool;
+
+    /// The legacy Yaml handling in Suricata treated strings with certain values as truthy
+    /// for boolean values, such as "yes", or "on", or "1". This method gives an interface
+    /// that is compatible with that logic.
+    fn is_true(&self) -> bool;
+}
+
+impl SuricataYaml for Yaml {
+    fn is_true(&self) -> bool {
+        match self {
+            Yaml::Boolean(v) => *v,
+            Yaml::String(v) => matches!(v.to_lowercase().as_str(), "1" | "yes" | "true" | "on"),
+            Yaml::Integer(v) => *v != 0,
+            _ => false,
+        }
+    }
+
+    fn set_int(&mut self, key: &str, value: i64) -> bool {
+        let parts: Vec<&str> = key.splitn(2, '.').collect();
+        let key = Yaml::from_str(parts[0]);
+        match self {
+            Yaml::Hash(hash) => {
+                if parts.len() == 1 {
+                    match hash.get(&key) {
+                        None | Some(Yaml::Integer(_)) => {
+                            hash.insert(key, Yaml::Integer(value));
+                            true
+                        }
+                        _ => false,
+                    }
+                } else {
+                    let entry = hash
+                        .entry(key)
+                        .or_insert_with(|| Yaml::Hash(LinkedHashMap::new()));
+                    entry.set_int(parts[1], value)
+                }
+            }
+            _ => false,
+        }
+    }
+
+    fn set_from_str(&mut self, key: &str, value: &str) -> bool {
+        let parts: Vec<&str> = key.splitn(2, '.').collect();
+        let key = Yaml::from_str(parts[0]);
+        match self {
+            Yaml::Hash(hash) => {
+                if parts.len() == 1 {
+                    match hash.get(&key) {
+                        None | Some(Yaml::Integer(_)) => {
+                            hash.insert(key, Yaml::from_str(value));
+                            true
+                        }
+                        _ => false,
+                    }
+                } else {
+                    let entry = hash
+                        .entry(key)
+                        .or_insert_with(|| Yaml::Hash(LinkedHashMap::new()));
+                    entry.set_from_str(parts[1], value)
+                }
+            }
+            _ => false,
+        }
+    }
 }
 
 pub fn get_node<'a>(node: &'a Yaml, key: &str) -> Option<&'a Yaml> {
@@ -47,7 +116,7 @@ pub fn get_node<'a>(node: &'a Yaml, key: &str) -> Option<&'a Yaml> {
 }
 
 pub fn set_default(yaml: Yaml) {
-    let mut default = DEFAULT.write().unwrap();
+    let mut default = GLOBAL.write().unwrap();
     *default = yaml;
 }
 
@@ -70,5 +139,25 @@ mod test {
 
         let node = get_node(&config, "nested.aaa").unwrap();
         assert_eq!(node.as_str().unwrap(), "bbb");
+    }
+
+    #[test]
+    fn test_set_int() {
+        let mut config = Yaml::Hash(LinkedHashMap::new());
+        config.set_int("foo", 1);
+        assert_eq!(config["foo"], Yaml::Integer(1));
+
+        config.set_int("foo", 2);
+        assert_eq!(config["foo"], Yaml::Integer(2));
+
+        config.set_int("bar.foo", 3);
+        assert_eq!(config["bar"]["foo"], Yaml::Integer(3));
+
+        config.set_int("bar.far", 4);
+        assert_eq!(config["bar"]["far"], Yaml::Integer(4));
+
+        // This will fail as bar is a hash, so we can't set it to a value of a different
+        // type.
+        assert_eq!(config.set_int("bar", 5), false);
     }
 }
