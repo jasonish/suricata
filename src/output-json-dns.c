68b166e15ee6074de38cb44b05058c179bdc47a3
Jason Ish <jason.ish@oisf.net>
2024-06-07 16:20:04 -0600

    dns: use common logging functions for alerts
    Bug: #6281

--- src/output-json-dns.c
+++ src/output-json-dns.c
@@ -247,69 +247,10 @@ typedef struct LogDnsLogThread_ {
     OutputJsonThreadCtx *ctx;
 } LogDnsLogThread;
 
-static JsonBuilder *JsonDNSLogQuery(void *txptr)
-{
-    JsonBuilder *queryjb = jb_new_array();
-    if (queryjb == NULL) {
-        return NULL;
-    }
-    bool has_query = false;
-
-    for (uint16_t i = 0; i < UINT16_MAX; i++) {
-        JsonBuilder *js = jb_new_object();
-        if (!SCDnsLogJsonQuery((void *)txptr, i, LOG_ALL_RRTYPES, js)) {
-            jb_free(js);
-            break;
-        }
-        jb_close(js);
-        has_query = true;
-        jb_append_object(queryjb, js);
-        jb_free(js);
-    }
-
-    if (!has_query) {
-        jb_free(queryjb);
-        return NULL;
-    }
-
-    jb_close(queryjb);
-    return queryjb;
-}
-
-static JsonBuilder *JsonDNSLogAnswer(void *txptr)
-{
-    if (!SCDnsLogEnabled(txptr, LOG_ALL_RRTYPES)) {
-        return NULL;
-    } else {
-        JsonBuilder *js = jb_new_object();
-        SCDnsLogJsonAnswer(txptr, LOG_ALL_RRTYPES, js);
-        jb_close(js);
-        return js;
-    }
-}
-
 bool AlertJsonDns(void *txptr, JsonBuilder *js)
 {
-    bool r = false;
-    jb_open_object(js, "dns");
-    if (SCDnsTxIsRequest(txptr)) {
-        r = SCDnsLogJson(txptr, LOG_ALL_RRTYPES, js);
-    } else {
-        JsonBuilder *qjs = JsonDNSLogQuery(txptr);
-        if (qjs != NULL) {
-            jb_set_object(js, "queries", qjs);
-            jb_free(qjs);
-            r = true;
-        }
-        JsonBuilder *ajs = JsonDNSLogAnswer(txptr);
-        if (ajs != NULL) {
-            jb_set_object(js, "answer", ajs);
-            jb_free(ajs);
-            r = true;
-        }
-    }
-    jb_close(js);
-    return r;
+    return SCDnsLogJson(
+            txptr, LOG_FORMAT_DETAILED | LOG_QUERIES | LOG_ANSWERS | LOG_ALL_RRTYPES, js);
 }
 
 static int JsonDnsLogger(ThreadVars *tv, void *thread_data, const Packet *p, Flow *f, void *alstate,
@@ -339,11 +280,9 @@ static int JsonDnsLogger(ThreadVars *tv, void *thread_data, const Packet *p, Flo
         return TM_ECODE_OK;
     }
 
-    jb_open_object(jb, "dns");
-    SCDnsLogJson(txptr, td->dnslog_ctx->flags, jb);
-    jb_close(jb);
-
-    OutputJsonBuilderBuffer(jb, td->ctx);
+    if (SCDnsLogJson(txptr, td->dnslog_ctx->flags, jb)) {
+        OutputJsonBuilderBuffer(jb, td->ctx);
+    }
     jb_free(jb);
 
     SCReturnInt(TM_ECODE_OK);
